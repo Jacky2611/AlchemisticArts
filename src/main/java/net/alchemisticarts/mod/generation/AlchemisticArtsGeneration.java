bb6830a74f18fdbc46123d447736d383953881d8
firew0rkz <firew0rkz666@gmail.com>
2014-11-26 14:14:28 +0100

    sadf

---
+++ src/main/java/net/alchemisticarts/mod/generation/AlchemisticArtsGeneration.java
@@ -0,0 +1,14 @@
+package net.alchemisticarts.mod.generation;
+
+import java.util.Random;
+
+import net.minecraft.src.IChunkProvider;
+
+import net.minecraft.src.World;
+
+import cpw.mods.fml.common.IWorldGenerator;
+
+
+public class AlchemisticArtsGeneration implements IWorldGenerator {
+
+}
