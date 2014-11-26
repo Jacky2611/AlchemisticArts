42be628baee85ab46249a5621a1175fdf05f8211
firew0rkz <firew0rkz666@gmail.com>
2014-11-26 14:18:11 +0100

    fixed errors

--- src/main/java/net/alchemisticarts/mod/generation/AlchemisticArtsGeneration.java
+++ src/main/java/net/alchemisticarts/mod/generation/AlchemisticArtsGeneration.java
@@ -2,13 +2,32 @@ package net.alchemisticarts.mod.generation;
 
 import java.util.Random;
 
-import net.minecraft.src.IChunkProvider;
-
-import net.minecraft.src.World;
-
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
 import cpw.mods.fml.common.IWorldGenerator;
 
 
-public class AlchemisticArtsGeneration implements IWorldGenerator {
+	public class AlchemisticArtsGeneration implements IWorldGenerator {
+		@Override
+			public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider){
+			switch(world.provider.dimensionId){
+	        case -1:
+	            generateNether(world, random, chunkX * 16, chunkZ * 16);
+	            break;
+	        case 0:
+	            generateSurface(world, random, chunkX * 16, chunkZ * 16);
+	            break;
+	        case 1:
+	            generateEnd(world, random, chunkX * 16, chunkZ * 16);
+	            break;
+	        }
+		}
 
-}
+		private void generateEnd(World world, Random random, int i, int j) {}
+
+		private void generateSurface(World world, Random random, int i, int j) {
+	        
+		}
+
+		private void generateNether(World world, Random random, int i, int j) {}
+	}
